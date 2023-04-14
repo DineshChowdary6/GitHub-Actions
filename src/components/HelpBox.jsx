3cdf016d9b1aa5699d60c50ce9810b5e705183ad
Dinesh Chowdary <dineshd385@gmail.com>
2023-04-14 16:34:20 -0400

    checking the same

--- src/components/HelpBox.jsx
+++ src/components/HelpBox.jsx
@@ -3,7 +3,7 @@ import './HelpBox.css';
 function HelpBox({ title, text }) {
   return (
     <article className="help-box">
-      <h4>{title}</h4>
+      <h2>{title}</h2>
       <p>{text}</p>
     </article>
   );
